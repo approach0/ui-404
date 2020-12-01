ef48555ce97e41849084397dd119daa7770d9929
t-k- <clock126@126.com>
2020-12-01 18:29:24 +0800

    first commit.

---
+++ daemon.js
@@ -0,0 +1,30 @@
+const express = require('express')
+const path = require('path')
+
+const app = express()
+app.use('/404', express.static('./static'))
+
+const port = 8080
+console.log('listening on ' + port)
+app.listen(port)
+
+const _404_page = path.resolve('./static/404.html')
+
+process.on('SIGINT', function() {
+  console.log('')
+  console.log('closing...')
+  process.exit()
+})
+
+app.get('/', async (req, res) => {
+  res.redirect('/search')
+
+}).get('/404', async (req, res) => {
+  res.sendFile(_404_page)
+  
+})
+
+app.use(function(req, res, next) {
+  res.sendFile(_404_page)
+
+})
